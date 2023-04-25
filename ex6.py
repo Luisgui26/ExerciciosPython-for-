0ce47e19a4c135d2bb615bf4b8205ce7450d8f88
Luis <luisguibarreto8@gmail.com>
2023-04-25 10:17:49 -0300

    Questão 6

--- ex6.py
+++ ex6.py
@@ -1,1 +1,14 @@
 #Criar um algoritmo que leia deznúmeros inteiros e informe o maior e o menor número.
+
+num = int(input("Informe um número: "))
+menor = num
+maior = num
+
+for n in range(9):
+    num = int(input("Informe um número: "))
+    if num > maior:
+     maior = num
+    if num < menor:
+        menor =  num
+print(f"Maior:{maior}")
+print(f"Menor:{menor}")
